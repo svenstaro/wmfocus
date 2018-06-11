32fee6199a4f78fae308ffcbfa97a3a4909d7d06
Sven-Hendrik Haase <svenstaro@gmail.com>
2018-06-11 05:09:28 +0200

    Showing text kind of works

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,19 @@
 #[macro_use]
 extern crate clap;
+extern crate cgmath;
 extern crate font_loader;
 extern crate fps_clock;
 extern crate glium;
 extern crate glium_text_rusttype as glium_text;
 
 use font_loader::system_fonts;
-use glium::{glutin, Surface, Display};
+use glium::{glutin, Surface};
+use std::collections::HashMap;
 use std::error::Error;
 
 use clap::{App, Arg};
 use glutin::os::unix::{WindowBuilderExt, XWindowType};
 use glutin::{Event, KeyboardInput, VirtualKeyCode, WindowEvent};
-use std::collections::HashMap;
 
 #[derive(Debug)]
 pub struct Window {
@@ -20,10 +21,16 @@ pub struct Window {
     pos: (i32, i32),
 }
 
+pub struct RenderWindow {
+    text_system: glium_text::TextSystem,
+    font: glium_text::FontTexture,
+    display: glium::Display,
+}
+
 #[derive(Debug)]
 pub struct AppConfig {
     pub font_family: String,
-    pub font_size: u8,
+    pub font_size: u32,
 }
 
 static HINT_CHARS: &'static str = "sadfjklewcmpgh";
@@ -43,7 +50,7 @@ fn is_truetype_font(f: String) -> Result<(), String> {
     if family.is_none() || size.is_none() {
         return Err("From font format".to_string());
     }
-    if let Err(e) = size.unwrap().parse::<u8>() {
+    if let Err(e) = size.unwrap().parse::<u32>() {
         return Err(e.description().to_string());
     }
     Ok(())
@@ -60,7 +67,7 @@ pub fn parse_args() -> AppConfig {
                 .long("font")
                 .takes_value(true)
                 .validator(is_truetype_font)
-                .default_value("DejaVu Sans Mono:14")
+                .default_value("DejaVu Sans Mono:72")
                 .help("Use a specific TrueType font with this format: family:size"),
         )
         .get_matches();
@@ -69,11 +76,19 @@ pub fn parse_args() -> AppConfig {
     let v: Vec<_> = font.split(":").collect();
     let (font_family, font_size) = (
         v.get(0).unwrap().to_string(),
-        v.get(1).unwrap().parse::<u8>().unwrap(),
+        v.get(1).unwrap().parse::<u32>().unwrap(),
     );
     AppConfig {
         font_family,
         font_size,
+    }
+}
+
+fn get_next_hint(current_hints: Vec<&String>, hint_chars: &str) -> String {
+    for c in hint_chars.chars() {
+        if !current_hints.contains(&&c.to_string()) {
+            return c.to_string();
+        }
     }
 }
 
@@ -83,14 +98,19 @@ fn main() {
     let font_family_property = system_fonts::FontPropertyBuilder::new()
         .family(&app_config.font_family)
         .build();
-    let mut loaded_font = system_fonts::get(&font_family_property);
-    if loaded_font.is_none() {
-        eprintln!("Family not found, falling back to first Monospace font");
-        let mut font_monospace_property = system_fonts::FontPropertyBuilder::new().monospace().build();
-        let sysfonts = system_fonts::query_specific(&mut font_monospace_property);
-        loaded_font = system_fonts::get(&font_monospace_property);
-        eprintln!("Falling back to font '{font}'", font=sysfonts[0]);
-    }
+    let (loaded_font, _) =
+        if let Some((loaded_font, index)) = system_fonts::get(&font_family_property) {
+            (loaded_font, index)
+        } else {
+            eprintln!("Family not found, falling back to first Monospace font");
+            let mut font_monospace_property =
+                system_fonts::FontPropertyBuilder::new().monospace().build();
+            let sysfonts = system_fonts::query_specific(&mut font_monospace_property);
+            eprintln!("Falling back to font '{font}'", font = sysfonts[0]);
+            let (loaded_font, index) =
+                system_fonts::get(&font_monospace_property).expect("Couldn't find suitable font");
+            (loaded_font, index)
+        };
 
     let windows = wm::get_windows();
 
@@ -98,35 +118,73 @@ fn main() {
     let mut fps = fps_clock::FpsClock::new(30);
 
     let mut events_loop = glutin::EventsLoop::new();
-    let mut displays = HashMap::new();
+    let mut render_windows = HashMap::new();
     for window in windows {
         let gwindow = glutin::WindowBuilder::new()
             .with_decorations(false)
             .with_always_on_top(true)
             .with_x11_window_type(XWindowType::Dialog)
-            .with_transparency(true)
-            .with_dimensions(50, 50);
+            .with_transparency(true);
         let context = glutin::ContextBuilder::new();
         let display = glium::Display::new(gwindow, context, &events_loop).unwrap();
-        let text_system = glium_text::TextSystem::new(&display);
-        // let font = glium_text::FontTexture::new(
-        //     &display,
-        //     File::open("font.ttf").unwrap(),
-        //     32,
-        //     glium_text::FontTexture::ascii_character_list(),
-        // ).unwrap();
         display.gl_window().set_position(window.pos.0, window.pos.1);
-        displays.insert(window.id, display);
+
+        let text_system = glium_text::TextSystem::new(&display);
+        let font = glium_text::FontTexture::new(
+            &display,
+            loaded_font.as_slice(),
+            app_config.font_size,
+            HINT_CHARS.chars(),
+        ).expect("Error loading font");
+        let render_window = RenderWindow {
+            text_system,
+            font,
+            display,
+        };
+        let hint = get_next_hint(render_windows.keys().collect(), HINT_CHARS);
+        render_windows.insert(hint.clone(), render_window);
+        let rw = &render_windows[&hint];
+        let text = glium_text::TextDisplay::new(&rw.text_system, &rw.font, &hint);
+        let (text_width, text_height) = (text.get_width(), text.get_height());
+        let ratio = text_height / text_width;
+        let window_width = 320;
+        let window_height = (window_width as f32 * ratio) as u32;
+        println!(
+            "text_width {} text_height {}, ratio {}, window_width {} window_height {}",
+            text_width, text_height, ratio, window_width, window_height
+        );
+        rw.display
+            .gl_window()
+            .set_inner_size(window_width, window_height);
     }
 
     let mut closed = false;
-    for (i, display) in &displays {
-        let mut target = display.draw();
-        target.clear_color(0.0 + 3.0 / (i + 1) as f32, 0.0, 1.0, 1.0);
-        target.finish().unwrap();
-    }
-
     while !closed {
+        for (hint, rw) in &render_windows {
+            let (w, h) = rw.display.get_framebuffer_dimensions();
+            let text = glium_text::TextDisplay::new(&rw.text_system, &rw.font, &hint);
+            let text_width = text.get_width();
+            println!("{} {}", w, h);
+
+            #[cfg_attr(rustfmt, rustfmt_skip)]
+            let matrix:[[f32; 4]; 4] = cgmath::Matrix4::new(
+                2.0 / text_width, 0.0, 0.0, 0.0,
+                0.0, 2.0 * (w as f32) / (h as f32) / text_width, 0.0, 0.0,
+                0.0, 0.0, 1.0, 0.0,
+                -1.0, -1.0, 0.0, 1.0f32,
+            ).into();
+
+            let mut target = rw.display.draw();
+            target.clear_color(0.0, 0.0, 1.0, 1.0);
+            glium_text::draw(
+                &text,
+                &rw.text_system,
+                &mut target,
+                matrix,
+                (1.0, 1.0, 0.0, 1.0),
+            ).unwrap();
+            target.finish().unwrap();
+        }
         events_loop.poll_events(|event| match event {
             Event::WindowEvent { event, .. } => match event {
                 WindowEvent::CloseRequested => closed = true,
